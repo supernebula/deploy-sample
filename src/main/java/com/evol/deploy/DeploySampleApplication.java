bc4109e1e8ef967c8f2a4524f1a53264637683c2
supernebula <supernebula@126.com>
2020-03-13 20:19:19 +0800

    init

---
+++ src/main/java/com/evol/deploy/DeploySampleApplication.java
@@ -0,0 +1,13 @@
+package com.evol.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploySampleApplication.class, args);
+    }
+
+}
